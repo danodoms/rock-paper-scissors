e79a2161391d94a443193165daecaba99779d489
danodoms <danodoms@gmail.com>
2023-08-27 21:21:23 +0800

    Added buggy scoreboard system

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let choices = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     //let choices = ["Rock", "Paper", "Scissors"];
     let randomNumber = Math.floor(Math.random() * 3);
-    let randomChoice = choices[randomNumber];
-    console.log("computer choice: " + randomChoice);
-    return randomChoice;
+    let randomComputerChoice = choices[randomNumber];
+    //console.log("computer choice: " + randomChoice);
+    return randomComputerChoice;
 }
 
 function playRound(playerSelection, computerSelection){
@@ -36,18 +38,49 @@ function playRound(playerSelection, computerSelection){
 
     let winner;
     if(checkWinner == -1 || checkWinner == 2){
-        winner = "Player wins!";
+        winner = "+++ Player wins! +++";
+        playerScore++;
     }else if(checkWinner == 1 || checkWinner == -2){
-        winner = "Computer wins!";
+        winner = "+++ Computer wins! +++";
+        computerScore++;
     }else if(checkWinner == 0){
-        winner = "Draw";
+        winner = "+++ Draw +++";
     }else{
         winner = "Invalid Choice, retry";
     }
-
+    //console.log("playRound() running");
     return winner;
 }
 
-let playerSelection = "scissors";
-console.log("player choice: " + playerSelection);
-console.log(playRound(playerSelection, getComputerChoice()));
+function game(){
+    for(let i=0; i<5; i++){
+        let drawFilter;          
+        do{
+            let playerSelection = prompt("Rock, Paper, Scissors");
+            let computerSelection = getComputerChoice();
+            drawFilter = playRound(playerSelection, computerSelection);
+
+            console.log("Round " + (i+1));
+            console.log("Player: " + playerSelection);
+            console.log("Computer: " + computerSelection);
+
+            console.log(playRound(playerSelection, computerSelection));
+            console.log(`Scoreboard | Player: ${playerScore} Computer: ${computerScore}`);
+            console.log("");
+        }while (drawFilter == "+++ Draw +++" || drawFilter == "+++ Invalid Choice, retry +++") 
+    }
+
+    if(playerScore > computerScore){
+        console.log("Overall Winner: Player");
+    }else{
+        console.log("Overall Winner: Computer");
+    }
+
+    playerScore = 0;
+    computerScore = 0;
+}
+//console.log(playRound("rock", getComputerChoice()));
+game();
+//let playerSelection = "scissors";
+//console.log("player choice: " + playerSelection);
+//console.log(playRound(playerSelection, getComputerChoice()));
