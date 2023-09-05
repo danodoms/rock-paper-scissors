fbc9085db45e6310a6636a8113c376ec29219b05
danodoms <danodoms@gmail.com>
2023-09-05 23:37:10 +0800

    added winner display

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 let choices = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
+const maxScore = 5;
+
+function displayScores(){
+
+}
+
+function displayRoundWinner(winner){
+    if(winner == 'Draw'){
+        winnerDisplay.textContent = winner;   
+    }else{
+        winnerDisplay.textContent = `${winner} wins!`;   
+    }
+}
+
+function determineOverallWinner(winner){
+    if (playerScore == maxScore){
+        display.textContent = `Player wins!`;
+    }else if(computerScore == maxScore){
+        display.textContent = `Computer wins!`;
+    }
+}
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
@@ -33,13 +54,13 @@ function playRound(playerSelection, computerSelection){
 
     let winner;
     if(checkWinner == -1 || checkWinner == 2){
-        winner = "+++ Player wins! +++";
+        winner = "Player";
         ++playerScore;
     }else if(checkWinner == 1 || checkWinner == -2){
-        winner = "+++ Computer wins! +++";
+        winner = "Computer";
         ++computerScore;
     }else if(checkWinner == 0){
-        winner = "+++ Draw +++";
+        winner = "Draw";
     }else{
         winner = "Invalid Choice, retry";
     }
@@ -49,43 +70,48 @@ function playRound(playerSelection, computerSelection){
     console.log("Computer: " + computerSelection);
     console.log(winner);
     console.log("");
+
+    displayRoundWinner(winner);
+    determineOverallWinner(winner);
     return winner;
 }
 
-function game(){
-    let roundCounter = 0;
-    let maxScore = 5;
+// function game(){
+//     let roundCounter = 0;
+//     let maxScore = 5;
 
-    while(playerScore < maxScore && computerScore < maxScore){
-        let playerSelection = prompt("Rock, Paper, Scissors");
-        let computerSelection = getComputerChoice();
+//     while(playerScore < maxScore && computerScore < maxScore){
+//         let playerSelection = prompt("Rock, Paper, Scissors");
+//         let computerSelection = getComputerChoice();
 
-        console.log("Round " + (roundCounter+1));
-        console.log("Player: " + playerSelection);
-        console.log("Computer: " + computerSelection);
+//         console.log("Round " + (roundCounter+1));
+//         console.log("Player: " + playerSelection);
+//         console.log("Computer: " + computerSelection);
 
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`Scoreboard | Player: ${playerScore} Computer: ${computerScore}`);
-        console.log("");
+//         console.log(playRound(playerSelection, computerSelection));
+//         console.log(`Scoreboard | Player: ${playerScore} Computer: ${computerScore}`);
+//         console.log("");
 
-        ++roundCounter;
-    }
+//         ++roundCounter;
+//     }
 
 
-    if(playerScore > computerScore){
-        console.log("Overall Winner: Player");
-    }else{
-        console.log("Overall Winner: Computer");
-    }
+//     if(playerScore > computerScore){
+//         console.log("Overall Winner: Player");
+//     }else{
+//         console.log("Overall Winner: Computer");
+//     }
 
-    playerScore = 0;
-    computerScore = 0;      
-}
+//     playerScore = 0;
+//     computerScore = 0;      
+// }
 
 const rockBtn = document.querySelector('.rock-btn');
 const paperBtn = document.querySelector('.paper-btn');
 const scissorsBtn = document.querySelector('.scissors-btn');
 const display = document.querySelector('.display');
+const winnerDisplay = document.querySelector('.winner-display');
+const scoreDisplay = document.querySelector('.score-display');
 
 rockBtn.addEventListener('click', () => {
     playRound('rock', getComputerChoice());
